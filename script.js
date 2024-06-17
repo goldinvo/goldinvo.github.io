ffac4a66d9aa45d765cbac7b41f4db6de6e35013
goldinvo <goldinbaokimvo@gmail.com>
2024-06-16 22:08:39 -0500

    restore (remove long/short toggle feature)

--- script.js
+++ script.js
@@ -7,15 +7,3 @@ function closeNav() {
     document.getElementById("myMenu").style.height = "0";
 }
   
-function toggle_ls() {
-    let button = document.getElementById("ls-button");
-    if (button.textContent == "condense") {
-        button.textContent = "expand"
-        document.getElementById("long").style.display = "none";
-        document.getElementById("short").style.display = "block";
-    } else {
-        button.textContent = "condense"
-        document.getElementById("long").style.display = "block";
-        document.getElementById("short").style.display = "none";
-    }
-}
