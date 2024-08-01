726ed1ffbac1842793f879fd4b3d2825e84fb3ad
goldinvo <goldinbaokimvo@gmail.com>
2024-08-01 11:41:44 -0500

    recipe nav dividers and foldable content

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function openNav() {
-    document.getElementById("myMenu").style.height = "100%";
+const collapseContentButton = document.getElementById('chevron');
+const content = document.getElementById('content')
+ 
+if (collapseContentButton) {
+    // Allow closed content to persist throughout pages (localStorage)
+    let collapseContent = JSON.parse(localStorage.getItem("collapseContent"));
+    if (collapseContent) {
+        collapseContentButton.classList.toggle('closed', true);
+        content.classList.toggle('closed', true);
+    }
+    
+    collapseContentButton.addEventListener('click', () => {
+        collapseContent = !collapseContent; // unset: !null => true
+        localStorage.setItem("collapseContent", JSON.stringify(collapseContent));
+        collapseContentButton.classList.toggle('closed', collapseContent);
+        content.classList.toggle('closed', collapseContent);
+    });
 }
-  
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("myMenu").style.height = "0";
-}
-  
