a0da37b6bc6413dce5cd0cde8fb82bd960a0c0e7
goldinvo <goldinbaokimvo@gmail.com>
2024-08-14 01:00:18 -0500

    check localstorage for collapse preference on pageshow

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const collapseContentButton = document.getElementById('chevron');
-const content = document.getElementById('content')
- 
-if (collapseContentButton) {
-    // Allow closed content to persist throughout pages (localStorage)
-    let collapseContent = JSON.parse(localStorage.getItem("collapseContent"));
-    if (collapseContent) {
-        collapseContentButton.classList.toggle('closed', true);
-        content.classList.toggle('closed', true);
+document.addEventListener('DOMContentLoaded', function() {
+    const collapseContentButton = document.getElementById('chevron');
+    const content = document.getElementById('content')
+     
+    if (collapseContentButton) {
+        const applyCollapse = () => {
+            // Allow closed content option to persist throughout pages (localStorage)
+            let collapseContent = JSON.parse(localStorage.getItem("collapseContent"));
+            collapseContentButton.classList.toggle('closed', Boolean(collapseContent));
+            content.classList.toggle('closed', Boolean(collapseContent));   
+        }
+        
+        collapseContentButton.addEventListener('click', () => {
+            collapseContent = !JSON.parse(localStorage.getItem("collapseContent")); // case local storage empty: !null => true
+            localStorage.setItem("collapseContent", JSON.stringify(collapseContent));
+            applyCollapse();
+        });
+        window.addEventListener('pageshow', applyCollapse); // Rerun if e.g. page loaded from cache
+
+        applyCollapse();
     }
-    
-    collapseContentButton.addEventListener('click', () => {
-        collapseContent = !collapseContent; // unset: !null => true
-        localStorage.setItem("collapseContent", JSON.stringify(collapseContent));
-        collapseContentButton.classList.toggle('closed', collapseContent);
-        content.classList.toggle('closed', collapseContent);
-    });
-}
+});
+
